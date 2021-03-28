e670619fae640e08cb0960a72c60fa6c394cfab2
Kirstabro <kirstabro@gmail.com>
2021-03-28 20:38:24 +0200

    [Add} ngx currency

--- wie-betaald-wat-app/src/app/app.module.ts
+++ wie-betaald-wat-app/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxCurrencyModule } from 'ngx-currency';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgxCurrencyModule
   ],
   imports: [
     BrowserModule,
