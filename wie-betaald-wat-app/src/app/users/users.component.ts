a2164afff43d7777990007a265bfd3bf85d14ee0
Kirstabro <kirstabro@gmail.com>
2021-03-29 02:45:48 +0200

    [ADD] Total amount added

--- wie-betaald-wat-app/src/app/users/users.component.ts
+++ wie-betaald-wat-app/src/app/users/users.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import {Person} from '../person';
+import { Person } from '../person';
 import { InputService } from '../services/input.service';
 import { EventhandlerService } from '../services/eventhandler.service'
 
@@ -13,6 +13,8 @@ import { EventhandlerService } from '../services/eventhandler.service'
 export class UsersComponent implements OnInit {
   users?: Person[];
 
+  totalAmount: number;
+
   constructor(private inputService: InputService,
     private eventHandler: EventhandlerService) { }
 
@@ -23,6 +25,17 @@ export class UsersComponent implements OnInit {
 
   getUsers(): void {
     this.inputService.getUsers()
-    .subscribe(users => this.users = users);
+      .subscribe(users => {
+        this.users = users
+        this.calculateTotal();
+      });
+  }
+
+  calculateTotal(): void {
+    let temp: number = 0;
+    this.users.map(user => {
+      temp += +user.value;
+    });
+    this.totalAmount = temp;
   }
 }
